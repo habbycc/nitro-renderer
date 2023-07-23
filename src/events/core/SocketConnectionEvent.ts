21198b1f5d450914e7587b3fa0c24de00ebd2ef5
Vanessa Barreiros <vanessa@monarchmoney.com>
2023-07-23 14:52:52 -0300

    Fix wrong variable

--- src/events/core/SocketConnectionEvent.ts
+++ src/events/core/SocketConnectionEvent.ts
@@ -1,11 +1,11 @@
-import { IConnection } from '../../api';
-import { NitroEvent } from './NitroEvent';
+import { IConnection } from "../../api";
+import { NitroEvent } from "./NitroEvent";
 
 export class SocketConnectionEvent extends NitroEvent {
-  public static CONNECTION_OPENED = 'SCE_OPEN';
-  public static CONNECTION_CLOSED = 'SCE_CLOSED';
-  public static CONNECTION_ERROR = 'SCE_ERROR';
-  public static CONNECTION_MESSAGE = 'SCE_MESSAGE';
+  public static CONNECTION_OPENED = "SCE_OPEN";
+  public static CONNECTION_CLOSED = "SCE_CLOSED";
+  public static CONNECTION_ERROR = "SCE_ERROR";
+  public static CONNECTION_MESSAGE = "SCE_MESSAGE";
 
   private _connection: IConnection;
   private _originalEvent: Event;
@@ -14,7 +14,7 @@ export class SocketConnectionEvent extends NitroEvent {
     super(type);
 
     this._connection = connection;
-    this._originalEvent = event;
+    this._originalEvent = originalEvent;
   }
 
   public get connection(): IConnection {
